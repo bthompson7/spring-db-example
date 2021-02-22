89e07df380a3d617356092803313edf73f6e5fa3
Ben Thompson <benjamin.philip.thompson@maine.edu>
2021-02-21 20:36:18 -0500

    added unit test for controller

---
+++ src/test/java/com/springdatabase/TestController.java
@@ -0,0 +1,29 @@
+package com.springdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class TestController {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/v1/getAllUser?name=Phil",
+				String.class)).contains("philemail123@gmail.com");
+	}
+	
+	
+}
