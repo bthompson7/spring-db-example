f79218c6926be1214cce88c884be291775c3470f
Ben Thompson <benjamin.philip.thompson@maine.edu>
2021-03-01 22:17:24 -0500

    validation tests now share a single object

--- src/test/java/com/springdatabase/TestValidation.java
+++ src/test/java/com/springdatabase/TestValidation.java
@@ -5,11 +5,15 @@ import com.springdatabase.validation.ValidateUser;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.aspectj.lang.annotation.Before;
+
 public class TestValidation {
 
+	ValidateUser u = new ValidateUser();
+
+	
 	@Test
 	public void testValidName() throws Exception {
-		ValidateUser u = new ValidateUser();
 		
 		boolean isNameValid = u.isNameValid("Ben Thompson");
 		
@@ -18,19 +22,13 @@ public class TestValidation {
 	
 	@Test
 	public void testValidName2() throws Exception {
-		ValidateUser u = new ValidateUser();
-		
 		boolean isNameValid = u.isNameValid("Ben-Philip Thompson");
-		
 		assertThat(isNameValid);
 	}
 	
 	@Test
 	public void testInvalidName() throws Exception {
-		ValidateUser u = new ValidateUser();
-		
 		boolean isNameValid = u.isNameValid("Ben$ Thompson");
-		
 		assertThat(isNameValid);
 	}
 	
@@ -38,47 +36,32 @@ public class TestValidation {
 	
 	@Test
 	public void testValidEmail() throws Exception {
-		ValidateUser u = new ValidateUser();
-		
 		boolean isEmailValid = u.isNameValid("somecoolemail@yahoo.org");
-		
 		assertThat(isEmailValid);
 	}
 	
 	@Test
 	public void testValidEmail2() throws Exception {
-		ValidateUser u = new ValidateUser();
-		
 		boolean isEmailValid = u.isNameValid("somecool-email@yahoo.org");
-		
 		assertThat(isEmailValid);
 	}
 	
 	@Test
 	public void testValidEmail3() throws Exception {
-		ValidateUser u = new ValidateUser();
-		
 		boolean isEmailValid = u.isNameValid("somecoolemail@some-site.org");
-		
 		assertThat(isEmailValid);
 	}
 	
 	
 	@Test
 	public void testInvalidEmail() throws Exception {
-		ValidateUser u = new ValidateUser();
-		
 		boolean isEmailValid = u.isNameValid("somecoolemail@yahoo........org");
-		
 		assertThat(!isEmailValid);
 	}
 	
 	@Test
-	public void testInvalidEmail2() throws Exception {
-		ValidateUser u = new ValidateUser();
-		
+	public void testInvalidEmail2() throws Exception {		
 		boolean isEmailValid = u.isNameValid("somecoolemail@yaho$o.org");
-		
 		assertThat(!isEmailValid);
 	}
 	
